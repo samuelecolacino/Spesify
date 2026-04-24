4c68f5340e9fd7c3670f5a5145f942aee8e557bf
Ahmad <ahmad08ali@gmail.com>
2026-04-24 10:14:05 +0200

    add index page with mock data

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,13 +3,14 @@ import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { useEffect } from 'react';
 import { useExpenseStore } from '../src/store/expenseStore';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
   const initialize = useExpenseStore((state) => state.initialize);
 
   useEffect(() => {
     initialize();
-  }, []);
+  }, [initialize]);
 
   return (
     <>
